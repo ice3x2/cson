a8208ac5ede57f58f6adf155b7600939062f0062
beom <ice3x2@gmail.com>
2023-11-29 07:28:02 +0900

    map 생성자 가져오기

--- src/main/java/com/snoworca/cson/serializer/SchemaFieldMap.java
+++ src/main/java/com/snoworca/cson/serializer/SchemaFieldMap.java
@@ -1,11 +1,30 @@
 package com.snoworca.cson.serializer;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.*;
 
 public class SchemaFieldMap extends SchemaField {
     SchemaFieldMap(TypeElement parentsTypeElement, Field field, String path) {
         super(parentsTypeElement, field, path);
     }
+
+
+
+    private static Constructor<?> constructorOfMap(Class<?> type) {
+        try {
+            if (type.isInterface() && Map.class.isAssignableFrom(type)) {
+                return HashMap.class.getConstructor();
+            } else if(type.isInterface())
+
+        } catch (NoSuchMethodException e) {
+            throw new CSONObjectException("Map type " + type.getName() + " has no default constructor.");
+        }
+
+
+
+    }
+
 
     @Override
     public SchemaNode copyNode() {
